f031cafbbf9eb99bb21cd00823c1add0ef67d639
vjauneau <viinzz49lol@gmail.com>
2019-01-10 17:42:35 +0100

    mise en place de algo

--- src/Applicatif/main.java
+++ src/Applicatif/main.java
@@ -1,14 +1,8 @@
 package Applicatif;
 
-import java.util.List;
-import java.util.Vector;
-
-import Class.Croisement;
-import Class.Evaluation;
+import Class.Algo;
 import Class.Individu;
-import Class.Population;
 import Class.Remplacement;
-import Class.RemplacementAleatoire;
 import Class.RemplacementRang;
 import Class.Selection;
 import Class.SelectionRang;
@@ -18,51 +12,13 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Individu myInd = new MyInd();
-		Population pop = new Population(5, myInd );
+		Selection selection = new SelectionRang();
+		Remplacement remplacement = new RemplacementRang();
 		
-		for(Individu ind : pop.getPopulation()) {
-			ind.setScore(ind.evaluer());
-			System.out.println(ind.toString());
-		}
+		Algo algo = new Algo();
 		
-		System.out.println("Evaluation");
-		
-		Evaluation eval = new Evaluation();
-		eval.evaluer(pop);
-		
-		for(Individu ind : pop.getPopulation()) {
-			System.out.println(ind.toString());
-		}
-		
-		System.out.println("Selection");
-		
-		SelectionRang selec = new SelectionRang();
-		
-		List<Vector<Individu>> list_parents = selec.selectionPaires(pop);
-		
-		for(Vector v : list_parents) {
-			System.out.println("Vector : (" + v.get(0) + ", " + v.get(1) + ")");
-		}
-		
-		for(Individu ind : pop.getPopulation()) {
-			System.out.println(ind.toString());
-		}
-		
-		System.out.println("Croisement : ");
-		Croisement croisement = new Croisement();
-		croisement.croisementIndividus(list_parents);
-		
-		
-		Population ajout = new Population(2, myInd);
-		eval.evaluer(ajout);
-		
-		Remplacement remplacement = new RemplacementAleatoire();
-		
-		pop = remplacement.remplacer(pop, ajout );
-		System.out.println("Remplacement");
-		for(Individu ind : pop.getPopulation()) {
-			System.out.println(ind.toString());
-		}
+		algo.run(myInd, selection, remplacement);
+
 		
 	}
 
--- src/Class/Algo.java
+++ src/Class/Algo.java
@@ -1,5 +1,58 @@
 package Class;
 
+import java.util.List;
+import java.util.Vector;
+
 public class Algo {
+	
+	public void run(Individu IndividuReference, Selection typeSelection, Remplacement typeRemplacement) {
+		
+		Population pop = new Population(5, IndividuReference );
+		
+		for(Individu ind : pop.getPopulation()) {
+			ind.setScore(ind.evaluer());
+			System.out.println(ind.toString());
+		}
+		
+		System.out.println("Evaluation");
+		
+		Evaluation eval = new Evaluation();
+		eval.evaluer(pop);
+		
+		for(Individu ind : pop.getPopulation()) {
+			System.out.println(ind.toString());
+		}
+		
+		System.out.println("Selection");
+		
+		Selection selec = typeSelection;
+		
+		List<Vector<Individu>> list_parents = selec.selectionPaires(pop);
+		
+		for(Vector v : list_parents) {
+			System.out.println("Vector : (" + v.get(0) + ", " + v.get(1) + ")");
+		}
+		
+		for(Individu ind : pop.getPopulation()) {
+			System.out.println(ind.toString());
+		}
+		
+		System.out.println("Croisement : ");
+		Croisement croisement = new Croisement();
+		List<Individu> enfants  = croisement.croisementIndividus(list_parents);
+		
+		
+		Population ajout = new Population(enfants);
+		eval.evaluer(ajout);
+		
+		Remplacement remplacement = typeRemplacement;
+		
+		pop = remplacement.remplacer(pop, ajout );
+		System.out.println("Remplacement");
+		for(Individu ind : pop.getPopulation()) {
+			System.out.println(ind.toString());
+		}
+		
+	}
 
 }
